19311c1201acf37f76c74fc49fb37a3b7954ce2e
duyanpeng <duyanpeng@duyanpengdeMacBook-Pro.local>
2018-06-26 14:31:44 +0800

    添加parcel

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import Button from './button.vue'
 
-Vue.component('g-button',Button)
+Vue.component('GButton',Button)
 new Vue({
     el:'#app',
+    mounted(){
+        console.log(1111)
+    }
 })
--- src/button.vue
+++ src/button.vue
@@ -15,7 +15,7 @@ export default {
 
   computed: {},
 
-  mounted: {},
+  mounted() {},
 
   methods: {}
 }
