fc38e73868d015e6c420d81fe5dea0df3a429f6b
duyanpeng <duyanpeng@duyanpengdeMacBook-Pro.local>
2018-06-26 11:16:34 +0800

    增加parcel出错

---
+++ src/button.vue
@@ -0,0 +1,25 @@
+<!--  -->
+<template>
+  <div>
+      <button>我是一个button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style scoped>
+</style>
